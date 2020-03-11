28d7c28af4374b014593c90abfd6aa4a47ece8fe
zoudaifa <daleyzou@gmail.com>
2020-03-11 23:50:54 +0800

    end

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//
 
 function Square(props) {
     return (
@@ -46,48 +47,68 @@ class Game extends React.Component {
         this.state={
             history: [{squares: Array(9).fill(null)}],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
 
-    handelClick(i) {
-        const history = this.state.history;
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length-1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]){
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : '0';
+        squares[i] = this.state.xIsNext ? "X" : "0";
         this.setState({
-            history: history.concat([{squares:squares}]),
+            history: history.concat([
+                {squares:squares}
+                ]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move)=>{
+            const desc = move?'Go to move #'+move:'Go to game start';
+            return (
+              <li key={move}>
+                  <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+              </li>
+            );
+        });
 
         let status;
         if (winner){
-            status = 'Winner: ' + winner;
+            status = "Winner: " + winner;
         }else {
-            status = 'Next player: ' + (this.state.xIsNext?'X':'0');
+            status = "Next player: " + (this.state.xIsNext?"X":"0");
         }
         return (
             <div className="game">
                 <div className="game-board">
                     <Board
-                        squates={current.squares}
-                        onClick={(i)=>handleClick(i)}
+                        squares={current.squares}
+                        onClick={i=>this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
+    }
+
+    jumpTo(step) {
+        this.setState({
+           stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
     }
 }
 
